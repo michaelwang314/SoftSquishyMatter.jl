1dca0d8668bc5daf770b3415c67906bfcaa86e8d
Michael Wang <58545313+michaelwang314@users.noreply.github.com>
2020-07-26 13:03:29 -0400

    Update Integrators.jl

--- src/Integrators.jl
+++ src/Integrators.jl
@@ -1,39 +1,47 @@
 export update_particles!
 
-#=
-Apply periodicity
-=#
+"""
+    wrap_position(position; period)
+
+Returns a new position after applying periodic boundary conditions.  The
+periodicity is given by 'period'.  If period < 0, then no periodic boundary 
+condition is applied.
+"""
 @inline function wrap_position(position::Float64; period::Float64)
     if period > 0.0
         return mod(position, period)
     end
     return position
 end
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+    update_particles!(brownian; period_x, period_y)
 
-#=
-Update particles with 'Brownian' integrator
-=#
-function update_particles!(integrator::Brownian; period_x::Float64 = -1.0, period_y::Float64 = -1.0)
-    @use_threads integrator.multithreaded for particle in integrator.particles
+Advances the particles stored under 'brownian.particles' by a timestep
+'brownian.dt'.  Apply periodic boundaries with periodicity 'period_x', 
+'period_y' if period_x > 0 or period_y > 0.
+"""
+function update_particles!(brownian::Brownian; period_x::Float64 = -1.0, period_y::Float64 = -1.0)
+    @use_threads brownian.multithreaded for particle in brownian.particles
         if !isnothing(particle.active_force)
-            update_active_force!(particle.active_force, particle; dt = integrator.dt)
+            update_active_force!(particle.active_force, particle; dt = brownian.dt)
 
             af_x, af_y = get_active_force(particle.active_force)
             particle.f_x += af_x
             particle.f_y += af_y
         end
 
-        dtγ_trans = integrator.dt / particle.γ_trans
-        amplitude = sqrt(2 * particle.D_trans * integrator.dt)
+        dtγ_trans = brownian.dt / particle.γ_trans
+        amplitude = sqrt(2 * particle.D_trans * brownian.dt)
         particle.x = wrap_position(particle.x + dtγ_trans * particle.f_x + amplitude * randn(), period = period_x)
         particle.y = wrap_position(particle.y + dtγ_trans * particle.f_y + amplitude * randn(), period = period_y)
 
         particle.f_x = 0.0
         particle.f_y = 0.0
 
-        if integrator.rotations
-            dtγ_rot = integrator.dt / particle.γ_rot
-            particle.θ += dtγ_rot * particle.t_θ + sqrt(2 * particle.D_rot * integrator.dt) * randn()
+        if brownian.rotations
+            dtγ_rot = brownian.dt / particle.γ_rot
+            particle.θ += dtγ_rot * particle.t_θ + sqrt(2 * particle.D_rot * brownian.dt) * randn()
             
             particle.t_θ = 0.0
         end
