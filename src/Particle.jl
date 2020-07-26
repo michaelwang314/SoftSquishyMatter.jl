a3178ff195ed96c47764c60756c00bbc2f904f27
Michael Wang <58545313+michaelwang314@users.noreply.github.com>
2020-07-26 16:51:55 -0400

    Update Particle.jl

--- src/Particle.jl
+++ src/Particle.jl
@@ -8,7 +8,7 @@ export remove_overlaps!
 """
     group_by_type(particles; ptype)
 
-Returns a group of particles with type(s) 'ptype'.
+Returns a group of particles with type(s) `ptype`.
 """
 function group_by_type(particles::Array{Particle, 1}; ptype::Union{Symbol, Array{Symbol, 1}})
     if !(ptype isa Array)
@@ -27,8 +27,8 @@ end
 """
     rectangular_lattice(; s_x, s_y, M_x, M_y)
 
-Generates a rectangular lattice with lattice constants 's_x', 's_y' in the x, y
-directions.  Each cell is duplicated 'M_x', 'M_y' times in the x, y directions.
+Generates a rectangular lattice with lattice constants `s_x, s_y` in the x, y
+directions.  Each cell is duplicated `M_x, M_y` times in the x, y directions.
 """
 function rectangular_lattice(; s_x::Float64, s_y::Float64, M_x::Int64, M_y::Int64)
     positions = Array{Array{Float64, 1}, 1}()
@@ -41,9 +41,9 @@ end
 """
     triangular_lattice(; s, M_x, M_y)
 
-Generates a triangular lattice with lattice constant 's'.  Each cell consists of
-points (0, 0) and (s / 2, s * sqrt(3) / 2).  The cells are duplicated
-'M_x', 'M_y' times in the x, y directions.  Note that there are other
+Generates a triangular lattice with lattice constant `s`.  Each cell consists of
+points `(0, 0)` and `(s / 2, s * sqrt(3) / 2)`.  The cells are duplicated
+`M_x, M_y` times in the x, y directions.  Note that there are other
 differently oriented triangular lattices.
 """
 function triangular_lattice(; s::Float64, M_x::Int64, M_y::Int64)
@@ -59,8 +59,8 @@ end
 """
     flat_edge(; from, to, spacing)
 
-Generates a line of points from point 'from' to point 'to' with approximate
-spacing 'spacing'.  Note that the spacing is automatically adjusted slightly so 
+Generates a line of points from point `from` to point `to` with approximate
+spacing `spacing`.  Note that the spacing is automatically adjusted slightly so 
 that points are equally spaced and include the start and end points.
 """
 function flat_edge(; from::Array{Float64, 1}, to::Array{Float64, 1}, spacing::Float64)
@@ -77,9 +77,9 @@ end
 """
     wedge(; angle, edge_length, spacing, orientation, tip)
 
-Generates a V-shaped wedge.  The angle of the wedge is 'angle' with each arm a 
-length 'edge_length'.  The points are separated by approximately 'spacing' so 
-that the start and end points are included.  'orientation' is the angle relative 
+Generates a V-shaped wedge.  The angle of the wedge is `angle` with each arm a 
+length `edge_length`.  The points are separated by approximately `spacing` so 
+that the start and end points are included.  `orientation` is the angle relative 
 to the x-axis and tip is the x, y location of the tip of the wedge.
 """
 function wedge(; angle::Float64, edge_length::Float64, spacing::Float64, orientation::Float64 = 0.0, tip::Array{Float64, 1})
@@ -95,10 +95,10 @@ function wedge(; angle::Float64, edge_length::Float64, spacing::Float64, orienta
 end
 
 """
-    remove_overlaps!
+    remove_overlaps!(positions; fixed_positions, minimum_distance, period_x, period_y)
 
-Removes points from 'positions' that are within a 'minimum_distance' of points 
-in 'fixed_positions'.  This is used to avoid large forces due to overlaps.
+Removes points from `positions` that are within a `minimum_distance` of points 
+in `fixed_positions`.  This is used to avoid large forces due to overlaps.
 """
 function remove_overlaps!(positions::Array{Array{Float64, 1}, 1}; fixed_positions::Array{Array{Float64, 1}, 1}, minimum_distance::Float64, period_x::Float64, period_y::Float64)
     indices_to_remove = Array{Int64, 1}()
