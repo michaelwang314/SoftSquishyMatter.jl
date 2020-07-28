1c3aa9e33d2b9adefeb3329749b1adcb76ec2cdc
Michael Wang <michael.wang.physics@gmail.com>
2020-07-28 13:32:34 -0400

    added temporary function

--- src/analysis/plotter.jl
+++ src/analysis/plotter.jl
@@ -1,7 +1,8 @@
 using Plots
 
-export plot_history!
 export plot_frame!
+export plot_frames!
+export animate_frames!
 
 """
     plot_frame!(particles; frame_size, xlim, ylim, colors, save_as)
@@ -40,18 +41,31 @@ function plot_frame!(particles::Array{Particle, 1}; frame_size::Tuple{Int64, Int
 end
 
 """
-    plot_history!(history; frame_size, xlim, ylim, colors, folder)
+    plot_frames!(history; frame_nums, frame_size, xlim, ylim, colors, folder)
 
 Plots each frame of `history`.
 """
-function plot_history!(history::Array{Array{Particle, 1}, 1}; frame_size::Tuple{Int64, Int64} = (600, 600), xlim::Array{Float64, 1}, ylim::Array{Float64, 1}, colors::Dict{Symbol, String} = Dict(:particle => "black"), folder::String = "frames/")
+function plot_frames!(history::Array{Array{Particle, 1}, 1}; frame_nums::Union{Array{Int64, 1}, Nothing} = nothing, frame_size::Tuple{Int64, Int64} = (600, 600), xlim::Array{Float64, 1}, ylim::Array{Float64, 1}, colors::Dict{Symbol, String} = Dict(:particle => "black"), folder::String = "frames/")
+    if isnothing(frame_nums)
+        frame_nums = [f for f = 1 : length(history)]
+    end
+
     println("")
     println("   +++++ GENERATING IMAGES +++++")
     println("")
-    for (f, particles) in enumerate(history)
-        plot_frame!(particles; frame_size = frame_size, xlim = xlim, ylim = ylim, colors = colors, save_as = string(folder, "Frame $f.png"))
+    for f in frame_nums
+        plot_frame!(history[f]; frame_size = frame_size, xlim = xlim, ylim = ylim, colors = colors, save_as = string(folder, "Frame $f.png"))
     end
     println("")
     println("   +++++ IMAGES GENERATED +++++")
     println("")
+end
+
+"""
+    animate_frames!(...)
+
+...
+"""
+function animate_frames!(history::Array{Array{Particle, 1}, 1}; frame_nums::Union{Array{Int64, 1}, Nothing} = nothing, frame_size::Tuple{Int64, Int64} = (600, 600), xlim::Array{Float64, 1}, ylim::Array{Float64, 1}, colors::Dict{Symbol, String} = Dict(:particle => "black"), save_as::String = "simulation.gif")
+    
 end
