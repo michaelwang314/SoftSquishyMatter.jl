7059c77873ee9aeea8d2d1c67d06a13a820522f3
Michael Wang <michael.wang.physics@gmail.com>
2020-08-06 00:42:52 -0400

    external forces, multithreaded

--- src/externalforces.jl
+++ src/externalforces.jl
@@ -3,11 +3,11 @@ export compute_external_force!
 """
     compute_external_force!(contant_force)
 
-Applies a constant force to select particles stored under 
+Applies a constant force to select particles stored under
 `constant_force.particles`.
 """
 function compute_external_force!(constant_force::ConstantForce)
-    for particle in constant_force.particles
+    @use_threads constant_force.multithreaded for particle in constant_force.particles
         particle.f_x += constant_force.f_x
         particle.f_y += constant_force.f_y
     end
@@ -20,7 +20,7 @@ Applies a harmonic potential to select particles stored under
 `harmonic_trap.particles`.
 """
 function compute_external_force!(harmonic_trap::HarmonicTrap)
-    for particle in harmonic_trap.particles
+    @use_threads harmonic_trap.multithreaded for particle in harmonic_trap.particles
         particle.f_x += -harmonic_trap.k_trap * (particle.x - harmonic_trap.x_center)
         particle.f_y += -harmonic_trap.k_trap * (particle.y - harmonic_trap.y_center)
     end
