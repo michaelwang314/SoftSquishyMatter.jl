af964cb139817455586ea6376a5d233733e4bd83
Michael Wang <michael.wang.physics@gmail.com>
2020-08-30 22:08:59 -0400

    change to simulation saving

--- src/runsimulation.jl
+++ src/runsimulation.jl
@@ -23,34 +23,43 @@ end
 Run the simulation.  `message_interval` (seconds) controls how often a time
 update is printed.  `save_as` is the file to which `simulation` is saved.
 """
-function run_simulation!(simulation::Simulation; message_interval::Float64 = 10.0, save_as::String = "")
+function run_simulation!(simulation::Simulation; message_interval::Float64 = 10.0, save_as::String = "", overwrite::Bool = true)
     print_message("SIMULATION STARTED")
     println("Number of threads available: ", Threads.nthreads())
     println("Number of particles: ", length(simulation.particles))
-    println("Number of bonds: ", length(simulation.bonds))
+    if length(simulation.bonds) > 0
+        println("Number of bonds: ", length(simulation.bonds))
+    end
+    if length(simulation.angles) > 0
+        println("Number of angles: ", length(simulation.angles))
+    end
     println("Description: ", simulation.descriptor)
     println("")
 
     period_x = simulation.periodic_in_x ? simulation.L_x : -1.0
     period_y = simulation.periodic_in_y ? simulation.L_y : -1.0
 
-    simulation.history = Array{NamedTuple{(:particles, :bonds), Tuple{Array{Particle, 1}, Array{NTuple{2, Particle}, 1}}}, 1}()
+    if overwrite
+        simulation.history = NamedTuple{(:particles, :bonds), Tuple{Array{Particle, 1}, Array{NTuple{2, Particle}, 1}}}[]
+        push!(simulation.history, deepcopy(simulation.things_to_save)) # save initial data
+    end
 
     print_message("SIMULATION PROGRESS")
     prev_step = 0
     time_elapsed = 0.0
     interval_start = time()
-    for step = 0 : simulation.num_steps
-        if step % simulation.save_interval == 0
-            push!(simulation.history, deepcopy(simulation.things_to_save))
-        end
-
+    for step = 1 : simulation.num_steps
         for interaction in simulation.interactions
             compute_interactions!(interaction; period_x = period_x, period_y = period_y)
         end
         for external_force in simulation.external_forces
             compute_external_forces!(external_force)
         end
+
+        if step % simulation.save_interval == 0
+            push!(simulation.history, deepcopy(simulation.things_to_save))
+        end
+
         for integrator in simulation.integrators
             update_particles!(integrator; period_x = period_x, period_y = period_y)
         end
@@ -61,15 +70,16 @@ function run_simulation!(simulation::Simulation; message_interval::Float64 = 10.
         interval_time = time() - interval_start
         if interval_time > message_interval || step == simulation.num_steps
             time_elapsed += interval_time
-            rate = (step + 1 - prev_step) / interval_time
+            rate = (step - prev_step) / interval_time
             println(hr_min_sec(time_elapsed), " | ",
-                    step + 1, "/", simulation.num_steps + 1, " (", round((step + 1) / simulation.num_steps * 100, digits = 1), "%)", " | ",
-                    round(rate, digits = 1), " steps/s", " | ",
+                    step, "/", simulation.num_steps, " (", round(step / simulation.num_steps * 100, digits = 1), "%) | ",
+                    round(rate, digits = 1), " steps/s | ",
                     hr_min_sec((simulation.num_steps - step) / rate))
             prev_step = step
             interval_start = time()
         end
     end
+    println("Average steps/s: ", round(simulation.num_steps / time_elapsed, digits = 1))
 
     if !isempty(save_as)
         save_simulation(simulation; save_as = save_as)
